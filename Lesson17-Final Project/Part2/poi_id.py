97383a89a09c4477942e56099070a78b8c5b23ee
Wenbin Guo <guowenbin90@gmail.com>
2021-10-09 11:22:50 -0400

    Update poi_id.py

--- Lesson17-Final Project/Part2/poi_id.py
+++ Lesson17-Final Project/Part2/poi_id.py
@@ -2,6 +2,7 @@
 
 import sys
 import pickle
+import matplotlib.pyplot as plt
 
 import numpy as np
 sys.path.append("../tools/")
@@ -12,13 +13,10 @@ import tester
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary']
-'''
-features_list = ['poi', 'salary','deferral_payments', 'total_payments', 'loan_advances', 
-'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses', 
-'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 
-'director_fees','shared_receipt_with_poi' ] # You will need to use more features
-'''
+#features_list = ['poi','salary']
+
+features_list = ['poi', 'deferred_income', 'total_stock_value', 'expenses', 'poi_mail_ratio']
+
 initial_features_list = ['poi','salary','deferral_payments', 'total_payments', 'loan_advances', 
 'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses', 
 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 
@@ -27,129 +25,114 @@ initial_features_list = ['poi','salary','deferral_payments', 'total_payments', '
 with open("final_project_dataset.pkl", "rb") as data_file:
     data_dict = pickle.load(data_file)
 
+num_samples = len(data_dict)
+print ("data set length:", num_samples)
+
 ### Task 2: Remove outliers
-identified_outliers = ['LAVORATO JOHN J', 'TOTAL', 'KAMINSKI WINCENTY J', 'BHATNAGAR SANJAY']  # iteration 1
-identified_outliers += ['KEAN STEVEN J', 'DERRICK JR. JAMES V', 'FREVERT MARK A', 'MARTIN AMANDA K'] # iteration 2
-identified_outliers += ['BELFER ROBERT'] # iteration 3
-#identified_outliers = ['TOTAL','FREVERT MARK A', 'LAVORATO JOHN J', 'WHALLEY LAWRENCE G', 'BAXTER JOHN C']
-print ("Original Length", len(data_dict))
-for outlier in identified_outliers:
-    data_dict.pop(outlier)    
+# First remove non-people entries
+data_dict.pop("TOTAL")
+data_dict.pop("THE TRAVEL AGENCY IN THE PARK")
 
-keys = data_dict.keys()
-#print(keys)
-print ("Length after Outlier", len(data_dict))  
 
-def replace(group, stds):
-    group[np.abs(group - group.mean()) > stds * group.std()] = (group.mean()+stds*group.std())
-    return group
 
-from sklearn import preprocessing
-for f in initial_features_list:
-    a=[data_dict[k][f] for k in keys]
-    #print(a[0])
-    for i in range(0,len(a)):
-        if a[i]=="NaN":
-            a[i]=0
-    a=np.array(a).astype(float)
-    # replacing anything more than 4 Std Dev with 4 Std Dev
-    a=replace(a,4)
-    # Scaling Data
-    ta_scaled = preprocessing.minmax_scale(a)
-    #print(ta_scaled[0])
-    i=0
-    for key in keys:
-        data_dict[key][f]=ta_scaled[i]
-        i=i+1
+# Iterate over dataset and use ad hoc filters to remove other outliers, if necessary
+salaryList = []
+clean_dict = {}
+for name, pdata in data_dict.items():
+    #print (pdata["from_messages"])
+    # Remove email freaks
+    if pdata["from_messages"]!="NaN":
+        if pdata["from_messages"]> 1000:
+            continue
+    clean_dict[name] = pdata
+    #print(clean_dict)
+
 
 ### Task 3: Create new feature(s)
-# Now Adding two new features
-for key in keys:
-    if data_dict[key]['from_poi_to_this_person']!=0:
-        data_dict[key]['percentage_from_poi']= (data_dict[key]['from_poi_to_this_person'])/float(data_dict[key]['to_messages'])
-    else:
-        data_dict[key]['percentage_from_poi']=0
-    if data_dict[key]['from_this_person_to_poi']==0:
-        data_dict[key]['percentage_to_poi']=0
-    else:
-        data_dict[key]['percentage_to_poi']= (data_dict[key]['from_this_person_to_poi'])/float(data_dict[key]['from_messages'])
-    data_dict[key]['ctc']=data_dict[key]['salary']+data_dict[key]['bonus']+data_dict[key]['exercised_stock_options']
-    #if data_dict[key]['salary']!=0:
-     #   data_dict[key]['bonus_to_salary'] = data_dict[key]['bonus'] / float(data_dict[key]['salary'])
-    #if data_dict[key]['total_payments']!=0:
-    #    data_dict[key]['bonus_to_total'] = data_dict[key]['bonus'] / float(data_dict[key]['total_payments'])
-
 ### Store to my_dataset for easy export below.
-my_dataset = data_dict
+
+#Lists used for visualization
+from_all=[]
+from_poi=[]
+m_ratio_list=[]
+
+for name, pdata in clean_dict.items():
+
+    pdata["poi_mail_ratio"] =0
+    if pdata['from_messages'] != "NaN":
+        from_all.append(float(pdata['from_messages']))
+        from_poi.append(float(pdata['from_poi_to_this_person']))
+        m_ratio = float(pdata['from_poi_to_this_person']) / float(pdata['from_messages'])
+        pdata["poi_mail_ratio"] = m_ratio
+        m_ratio_list.append(m_ratio)
+
+#print ("m ratios", m_ratio_list)
+
+plt.scatter(from_all, from_poi)
+plt.xlabel("all emails")
+plt.ylabel("from POI")
+
+
+my_dataset = clean_dict
+#print(my_dataset)
 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
+import pandas as pd
+df = pd.DataFrame.from_dict(my_dataset, orient='index') # Names as index
+
+
+
 ### Task 4: Try a varity of classifiers
-### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
-
-# Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf_GNB = GaussianNB()
-
-from sklearn.svm import SVC
-#clf = SVC(kernel='rbf',C=100)
-clf_SVC = SVC()
-
+# Switch to decision tree
 from sklearn.tree import DecisionTreeClassifier
-clf_DT= DecisionTreeClassifier()
+clf_DT= DecisionTreeClassifier(min_samples_split=6)
 
-from sklearn.cluster import KMeans
-clf_Kmeans = KMeans(n_clusters=2)
-#print(clf_Kmeans.get_params())
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
-### using our testing script. Check the tester.py script in the final project
-### folder for details on the evaluation method, especially the test_classifier
-### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
 from sklearn.model_selection import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
+'''
+from time import time
+t0 = time()
+clf_DT.fit(features_train, labels_train)
+print ("training time:", round(time()-t0, 3), "s")
+'''
 
-### Task 6: Dump your classifier, dataset, and features_list so anyone can
-### check your results. You do not need to change anything below, but make sure
-### that the version of poi_id.py that you submit can be run on its own and
-### generates the necessary .pkl files for validating your results.
+### Task 6: Dump your classifier, dataset, and features_list so anyone can check your results.
+'''
+from sklearn.model_selection import GridSearchCV
+param_grid = {'min_samples_split': np.arange(2, 10)}
+tree = GridSearchCV(DecisionTreeClassifier(), param_grid)
+tree.fit(features_train, labels_train)
+print(tree.best_params_) # get {'min_samples_split': 6}
+'''
 
-#dump_classifier_and_data(clf, my_dataset, features_list)
+from sklearn.feature_selection import RFECV
 
-print("GaussianNB:")
-tester.dump_classifier_and_data(clf_GNB, my_dataset, features_list)
-tester.main()
-print("SVC:")
-tester.dump_classifier_and_data(clf_SVC, my_dataset, features_list)
-tester.main()
+rfecv = RFECV(estimator=clf_DT, step=1, scoring="f1")
+rfecv.fit(features_train, labels_train)
+
+
+print ("optimal number of features",rfecv.n_features_)
+print ("Now Printing Features Priority")
+for i in range(0,len(rfecv.ranking_)): 
+    print (features_list[i+1], rfecv.ranking_[i])
+print (rfecv.ranking_)
+# Plot number of features VS. cross-validation scores
+plt.figure()
+plt.xlabel("Number of features selected")
+plt.ylabel("Cross validation score (f1 score)")
+plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_)
+plt.show()
+
+
 print("Decision Tree:")
 tester.dump_classifier_and_data(clf_DT, my_dataset, features_list)
 tester.main()
-print("Kmeans")
-tester.dump_classifier_and_data(clf_Kmeans, my_dataset, features_list)
-tester.main()
 
-#Trying Parameter Tuning to get Best Params
-#from sklearn.model_selection import GridSearchCV
-#param_grid = {'min_samples_split': np.arange(2, 10)}
-#tree = GridSearchCV(DecisionTreeClassifier(), param_grid)
-#tree.fit(features_train, labels_train)
-#print(tree.best_params_)
-
-
-#clf_DT.fit(features_train,labels_train)
-#pred_DT=clf_DT.predict(features_test)
-#from sklearn.metrics import classification_report
-#target_names = ['Not PoI', 'PoI']
-
-#print( classification_report(labels_test, pred_DT, target_names=target_names))
